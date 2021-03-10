afad05b1ff77b6d1ff3a711f27261565bc0f92dc
Ciaran Evans <ciaran@developmentseed.org>
2021-03-10 15:07:54 +0000

    Run formatting

--- cdk/app.py
+++ cdk/app.py
@@ -2,9 +2,7 @@
 import os
 
 from aws_cdk import core
-
 from bakery_stack import BakeryStack
-
 
 app = core.App()
 
@@ -13,14 +11,14 @@ identifier = os.environ["IDENTIFIER"]
 BakeryStack(
     scope=app,
     construct_id=f"pangeo-forge-aws-bakery-{identifier}",
-    identifier=identifier
+    identifier=identifier,
 )
 
 for k, v in {
     "Project": "pangeo-forge-aws-bakery",
     "Stack": identifier,
     "Client": "pangeo-forge",
-    "Owner": os.environ["OWNER"]
+    "Owner": os.environ["OWNER"],
 }.items():
     core.Tags.of(app).add(k, v, apply_to_launched_instances=True)
 
--- cdk/bakery_stack.py
+++ cdk/bakery_stack.py
@@ -2,8 +2,9 @@ from aws_cdk import core
 
 
 class BakeryStack(core.Stack):
-
-    def __init__(self, scope: core.Construct, construct_id: str, identifier: str, **kwargs) -> None:
+    def __init__(
+        self, scope: core.Construct, construct_id: str, identifier: str, **kwargs
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
