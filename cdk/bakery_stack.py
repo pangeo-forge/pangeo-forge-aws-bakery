2d8f704f4da13242dfca8922b90fd7b708fa1482
Ciaran Evans <ciaran@developmentseed.org>
2021-03-18 13:26:55 +0000

    Update CDK

--- cdk/bakery_stack.py
+++ cdk/bakery_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import core
+from aws_cdk import core, aws_ec2
 
 
 class BakeryStack(core.Stack):
@@ -7,4 +7,18 @@ class BakeryStack(core.Stack):
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        vpc = aws_ec2.Vpc(
+            self,
+            id=f"bakery-vpc-{identifier}",
+            cidr="10.0.0.0/16",
+            enable_dns_hostnames=True,
+            enable_dns_support=True,
+            nat_gateways=0,
+            subnet_configuration=[
+                aws_ec2.SubnetConfiguration(
+                    name="PublicSubnet1",
+                    subnet_type=aws_ec2.SubnetType.PUBLIC
+                )
+            ],
+            max_azs=3
+        )
